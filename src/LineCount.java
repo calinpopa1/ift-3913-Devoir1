e3a86a0e1fea92080053c2c1306f5d9a7fcdb6dc
calinpopa1 <66457665+calinpopa1@users.noreply.github.com>
2022-02-08 17:19:12 -0500

    ajout de commentaires dans tache 1

--- src/LineCount.java
+++ src/LineCount.java
@@ -12,46 +12,28 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**rth
- * This class  counts the number of source code lines by excluding comments, in a Java file
- * The pseudocode is as below
- * 
- * Initial: Set count = 0, commentBegan = false
- * Start: Read line
- * Begin: If line is not null, goto Check, else goto End
- * Check: If line is a trivial line(after trimming, either begins with // or is ""), goto Start
- *        If commentBegan = true
- *             if comment has not ended in line 
- *                goto Start
- *              else 
- *                line = what remains in the line after comment ends
- *                commenBegan = false
- *                if line is trivial
- *                   goto Start
- * 		  If line is a valid source code line, count++
- *        If comment has begun in the line, set commentBegan = true
- *        goto Start
- * End: print count
+/**
+ * Cette méthode compte le nombre de lignes de codes d'une classe java en excluant les commentaires. Elle prend 
+ * en entrée le chemin (Path) du fichier java. Cette méthode inclue les sous-méthoodes commentBegan et commentEnded
  */
 
- 
-public class LineCount {
+ public class LineCount {
 	public static float getNumberOfLines(String location)
 			throws IOException {
-		try (BufferedReader bReader = new BufferedReader(new FileReader(location))) {
+		try (BufferedReader bRead = new BufferedReader(new FileReader(location))) {
 			float count = 0;
-			boolean commentBegan = false;
+			boolean commBegan = false;
 			String line = null;
 
-			while ((line = bReader.readLine()) != null) {
+			while ((line = bRead.readLine()) != null) {
 				line = line.trim();
 				if ("".equals(line) || line.startsWith("//")) {
 					continue;
 				}
-				if (commentBegan) {
+				if (commBegan) {
 					if (commentEnded(line)) {
 						line = line.substring(line.indexOf("*/") + 2).trim();
-						commentBegan = false;
+						commBegan = false;
 						if ("".equals(line) || line.startsWith("//")) {
 							continue;
 						}
@@ -62,55 +44,49 @@ public class LineCount {
 				count++;
 				
 				if (commentBegan(line)) {
-					commentBegan = true;
+					commBegan = true;
 				}
 			}
 			return count;
 		}
 	}
 	
-	/**
-	 * 
-	 * @param line
-	 * @return This method checks if in the given line a comment has begun and has not ended
-	 */
+	
+	//Cette méthode vérifie si un commentaire a commencé et si il n'est pas encore terminé
 	private static boolean commentBegan(String line) {
-		// If line = /* */, this method will return false
-		// If line = /* */ /*, this method will return true
-		int index = line.indexOf("/*");
-		if (index < 0) {
+		// Si line = /* */, la methode retourne faux
+		// Si line = /* */ /*, la methode retourne vrai
+		int i = line.indexOf("/*");
+		if (i < 0) {
 			return false;
 		}
-		int quoteStartIndex = line.indexOf("\"");
-		if (quoteStartIndex != -1 && quoteStartIndex < index) {
-			while (quoteStartIndex > -1) {
-				line = line.substring(quoteStartIndex + 1);
-				int quoteEndIndex = line.indexOf("\"");
-				line = line.substring(quoteEndIndex + 1);
-				quoteStartIndex = line.indexOf("\"");
+		int startIndex = line.indexOf("\"");
+		if (startIndex != -1 && startIndex < i) {
+			while (startIndex > -1) {
+				line = line.substring(startIndex + 1);
+				int endIndex = line.indexOf("\"");
+				line = line.substring(endIndex + 1);
+				startIndex = line.indexOf("\"");
 			}
 			return commentBegan(line);
 		}
-		return !commentEnded(line.substring(index + 2));
+		return !commentEnded(line.substring(i + 2));
 	}
 	
-	/**
-	 * 
-	 * @param line
-	 * @return This method checks if in the given line a comment has ended and no new comment has not begun
-	 */ 
+	
+	// Cette methode vérifie si un commentaire est terminé et si il n'ya pas un autre commentaire qui a commencé.
 	private static boolean commentEnded(String line) {
-		// If line = */ /* , this method will return false
-		// If line = */ /* */, this method will return true
-		int index = line.indexOf("*/");
-		if (index < 0) {
+		// Si line = */ /* , la méthode retourne faux.
+		// Si line = */ /* */, la méthode retourne vrai.
+		int i = line.indexOf("*/");
+		if (i < 0) {
 			return false;
 		} else {
-			String subString = line.substring(index + 2).trim();
-			if ("".equals(subString) || subString.startsWith("//")) {
+			String subStr = line.substring(i + 2).trim();
+			if ("".equals(subStr) || subStr.startsWith("//")) {
 				return true;
 			}
-			if(commentBegan(subString))
+			if(commentBegan(subStr))
 			{
 				return false;
 			}
@@ -121,49 +97,44 @@ public class LineCount {
 		}
 	}
 
+	//Cette fonction vérifie chaque ligne d'une classe java et retourne le nombre
+	//de commentaires de la classe. Cette méthode prend le chemin de la classe en entrée
 	public static float getNumberOfCommentLines(String location) throws FileNotFoundException{
-	BufferedReader bReader= new BufferedReader(new FileReader(location));
+	BufferedReader bRead= new BufferedReader(new FileReader(location));
 	String line = "";
     float count = 0;
     try {
         
-        while ((line = bReader.readLine()) != null) {
+        while ((line = bRead.readLine()) != null) {
 			
 			if (line.contains("//")) {
                 count++;
             } else if (line.contains("/*")) {
-                //count++;
-                while (!line.contains("*/") && !(line = bReader.readLine()).contains("*/")){
+                while (!line.contains("*/") && !(line = bRead.readLine()).contains("*/")){
 					count++;
 				};
             }
         }
-        bReader.close();
-    //} catch (FileNotFoundException e) {
-    //    e.printStackTrace();
+        bRead.close();
     } catch (IOException e) {
         e.printStackTrace();
     }
 
     return count;
 	}
-	
+
+
+	//Cette méthode retourne la densité de commentaire d'une classe.
 	public static float getClassCommentDensity(String location) throws IOException {
-		BufferedReader b1 = new BufferedReader(new FileReader(location));
 
 		float CommsC= getNumberOfCommentLines(location);
 		float LinesC= getNumberOfLines(location);
 		float densite= CommsC/LinesC;
-		
-		
-		//int lol= getNumberOfCommentLines(bReader);
-		//bReader.reset();
-		//int lol1 = getNumberOfLines(bReader);
-		
-
 		return densite;
 	}
 
+	//Cette methode retourne le nombre de lignes de code d'un paquet. On doit lui donner le chemin
+	//du paquet en entrée.
 	public static float getNumberOfLinesPackage(String location) throws IOException{
 		Path path=Paths.get(location);
 		float count=0;
@@ -184,6 +155,7 @@ public class LineCount {
 
 	}
 
+	//Cette méthode retourne le nombre de commentaires d'un paquet.
 	public static float getNumberOfCommentLinesPackage(String location) throws IOException{
 		Path path=Paths.get(location);
 		float count=0;
@@ -204,19 +176,13 @@ public class LineCount {
 
 	}
 
+	//Cette méthode retourne la densité de commentaire d'un paquet.
 	public static float getPackageCommentDensity(String location) throws IOException {
-		//BufferedReader b1 = new BufferedReader(new FileReader(location));
 
 		float CommsP= getNumberOfCommentLinesPackage(location);
 		float LinesP= getNumberOfLinesPackage(location);
 		float densite= CommsP/LinesP;
-		
-		
-		//int lol= getNumberOfCommentLines(bReader);
-		//bReader.reset();
-		//int lol1 = getNumberOfLines(bReader);
-		
-
+	
 		return densite;
 	}
 
