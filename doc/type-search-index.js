cc90da5f51c2bb27f93ec71799016bd512388564
kadina21 <93254604+kadina21@users.noreply.github.com>
2022-02-10 11:53:20 -0500

    javadoc generated

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"App"},{"p":"<Unnamed>","l":"LineCount"},{"p":"<Unnamed>","l":"Sorties"}];updateSearchResults();
